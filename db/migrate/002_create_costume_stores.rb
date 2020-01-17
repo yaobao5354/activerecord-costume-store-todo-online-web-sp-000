44b7fbcac6d53dfeccb44f45c0e29232fb37c137
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 22:28:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+   create_table :costumes do |t|
+     t.string :name
+     t.float :price
+     t.string :size
+     t.string :image_url
+     t.timestamps
+   end
+  end
+end
