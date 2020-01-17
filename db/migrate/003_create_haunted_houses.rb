b8046b06e4fda4a921dcedda138b93d49674a0ec
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 22:39:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-   create_table :costumestores do |t|
+   create_table :haunted_houses do |t|
      t.string :name
      t.string :location
      t.string :theme
