1d46fe06a06f19d0ea70c8018584882c547970e7
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 23:00:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
      t.string :location
      t.string :theme
      t.float :price
-     t.boolean :family_member
+     t.boolean :family_friendly
      t.datetime :opening_date
      t.datetime :closing_date
      t.string :description
