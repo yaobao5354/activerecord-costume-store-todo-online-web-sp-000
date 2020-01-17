ab49bb49e01822536cc04c290e3831002c1b7ee6
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 23:02:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
    create_table :costume_stores do |t|
      t.string :name
      t.string :location
-     t.integer :custom_inventory
+     t.integer :custome_inventory
      t.integer :number_of_employees
      t.boolean :in_business
      t.datetime :opening_time
