029efcfb107106f2e138b7bc36748587c82f4d6f
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 23:00:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
@@ -19,16 +19,6 @@ ActiveRecord::Schema.define(version: 2) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "costumestores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "custom_inventory"
-    t.integer "number_of_employees"
-    t.boolean "in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
   end
 
 end
